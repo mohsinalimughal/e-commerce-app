0a08d7c1ca691d56ab9a6b081e4ba680fab9cf22
mohsinalimughal <mohsinalimughal112233445566@gmail.com>
2024-12-10 05:20:24 -0800

    ok

--- cart.js
+++ cart.js
@@ -3,6 +3,7 @@ console.log(cart)
 
 const cartitems = document.querySelector("#cart-items")
 
+const renderitems = ()=>{
 cart.map((item ,index)=>{
     cartitems.innerHTML += `
                        <div class="col">
@@ -12,21 +13,40 @@ cart.map((item ,index)=>{
                         <h5 class="card-title text-truncate">${item.title}</h5>
                         <p class="card-text text-success fw-bold">$${item.price}</p>
                         <div class="d-flex justify-content-between align-items-center mb-3">
-                            <button class="btn btn-outline-secondary btn-sm">-</button>
+                            <button class="btn btn-outline-secondary btn-sm" onclick="minusquantity(${index})">-</button>
                             <input type="number" class="form-control form-control-sm text-center" value="${item.quantity}" style="width: 60px;" min="1">
-                            <button class="btn btn-outline-secondary btn-sm">+</button>
+                            <button class="btn btn-outline-secondary btn-sm" onclick="addquantity(${index})">+</button>
                         </div>
                         <button class="btn btn-danger btn-sm w-100" onclick="removefromcart(${index})">Remove from Cart</button>
                     </div>
                 </div>
             </div>
-
     `
 })
-
+}
+renderitems()
 const removefromcart = (index)=>{
            
        cart.splice(index,1)
+       cartitems.innerHTML =``
+       renderitems()
        localStorage.setItem("cart",JSON.stringify(cart))
 
+}
+
+const addquantity = (index)=>{
+    console.log("add clicked",index)
+    cart[index].quantity++
+    console.log(cart[index])
+    cartitems.innerHTML =``
+    renderitems()
+    localStorage.setItem("cart",JSON.stringify(cart))
+}
+const minusquantity = (index)=>{
+    console.log("minus clicked",index)
+    cart[index].quantity--
+    console.log(cart[index])
+    cartitems.innerHTML =``
+    renderitems()
+    localStorage.setItem("cart",JSON.stringify(cart))
 }
